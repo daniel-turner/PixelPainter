fbd750973f354583a29e80b6b2889fce4d855a16
danturne <danturne@yahoo.com>
2015-06-15 16:05:34 -1000

    working!

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,8 +1,34 @@
 window.onload = function() {
 
-  var picker = new Grid(15,15);
-  var pickerContainer = document.getElementById('toolMenu');
+  var selectedColor = "#FFFFFF";
 
-  picker.draw(pickerContainer);
+  function pickerClick() {
+
+    selectedColor = this.style.backgroundColor;
+
+    console.log(selectedColor);
+
+  };
+
+  function canvasClick() {
+
+    this.style.backgroundColor = selectedColor;
+
+  };
+
+  var colors = [ "#AA3C39", "#FFACAA", "#D46D6A", "#801815", "#550200", "#AA9639",
+    "#FFF0AA", "#D4C26A", "#806D15", "#554600", "#403075", "#877CB0", "#605292",
+    "#261758", "#12073B", "#2D8632", "#86C98A", "#54A759", "#116416", "#004304"
+  ];
+
+  var grid = new Grid(5,4);
+  var container = document.getElementById("toolMenu");
+
+  grid.draw(container, pickerClick,colors);
+
+  grid = new Grid(30,40);
+  container = document.getElementById("canvas");
+
+  grid.draw(container,canvasClick);
 }
 
--- js/grid.js
+++ js/grid.js
@@ -3,33 +3,47 @@ function Grid(height,width) {
   this.height = height;
   this.width = width;
 
-  this.draw = function(parent) {
+  this.draw = function(parent,clickEvent, colors) {
 
     // var ratioW = Math.floor((window.innerWidth || document.documentElement.offsetWidth) / this.width);
     // var ratioH = Math.floor((window.innerHeight || document.documentElement.offsetHeight) / this.height);
 
-    var ratioW = 600 / this.width;
-    var ratioH = 600 / this.height;
+    var style = getComputedStyle(parent);
+
+    // var ratioW = 600 / this.width;
+    // var ratioH = 600 / this.height;
+
+    var ratioW = Math.floor((style.width.slice(0,style.width.length-2) * 0.8) / this.width);
+    //var ratioW = Math.floor(style.width.slice(0,style.width.length-2) / (this.width+2));
+    //var ratioH = Math.floor(style.height.slice(0,style.height.length-2) / this.height);
 
     var grid = document.createElement('div');
     grid.className = 'grid';
-    grid.style.width = (ratioW * width) + 'px';
-    grid.style.height = (ratioH * height) + 'px';
+    //grid.style.width = (ratioW * this.width) + 'px';
+    grid.style.width = style.width;
+    grid.style.height = (ratioW * this.height) + 'px';
+
+    var colorCount = 0;
 
     for (var i = 0; i < this.height; i++) {
 
       var row = document.createElement('div');
       row.className = 'row';
-
       row.style.clear = "left";
 
         for (var p = 0; p < this.width; p++) {
             var cell = document.createElement('div');
             cell.className = 'cell';
-            cell.style.height = (20) + 'px';
-            cell.style.width = (20) + 'px';
-            cell.style.float = 'left';
+            cell.style.height = ratioW + 'px';
+            cell.style.width = ratioW + 'px';
+            if(colors !== undefined) {
+
+              cell.style.backgroundColor = colors[colorCount];
+              colorCount++;
+            }
             cell.style.border = '1px solid black';
+
+            cell.addEventListener('click', clickEvent);
             row.appendChild(cell);
         }
 
